cca8b6990de7e25693b1289cb6d6a90293a66c47
monochromegane <dev.kuro.obi@gmail.com>
2014-05-17 22:07:36 +0900

    test cgo.

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/monochromegane/terminal"
-	"bufio"
 	"os"
+	"os/user"
 )
 
 func main() {
+
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Print("err = %\n", err)
+		return
+	}
+	fmt.Printf(usr.HomeDir)
 
 	if terminal.IsTerminal(os.Stdin) {
 		fmt.Println("Stdin is from Terminal.")
 	} else {
 		fmt.Println("Stdin isn't from Terminal.")
-		reader := bufio.NewReader(os.Stdin);
-		input,_:= reader.ReadString('\n');
-		fmt.Printf("----\n%s----\n", input);
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		fmt.Printf("----\n%s----\n", input)
 	}
 
 	if terminal.IsTerminal(os.Stdout) {
